3123cd34cdc7ac0a53da4a246745288a39bc3dbb
Christian Witts <cwitts@gmail.com>
2016-05-31 16:41:51 +0200

    Added some console messages to the build script.

--- build.sh
+++ build.sh
@@ -20,18 +20,23 @@ function help {
 }
 
 function build {
+    consoleLog "Building for version ${1}"
     BUILD_ROOT="bin/releases/${1}"
-    mkdir -p BUILD_ROOT/{linux_64,darwin}
+    mkdir -p ${BUILD_ROOT}/{linux_64,darwin}
+
+    consoleLog "Building Linux amd64 binary"
     GOOS=linux; GOARCH=amd64 go build \
         -ldflags="-X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse HEAD` -X main.version=${1}" \
         -o ${BUILD_ROOT}/linux_64/service-generator
 
+    consoleLog "Building Darwin amd64 binary"
     GOOS=darwin; GOARCH=amd64 go build \
         -ldflags="-X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse HEAD` -X main.version=${1}" \
         -o ${BUILD_ROOT}/darwin/service-generator
 }
 
 function buildRelease {
+    consoleLog "Generating releases for version ${1}"
     RELEASE_ROOT="bin/releases/${1}"
     (cd $RELEASE_ROOT; tar -czvf "service-generator_darwin-${1}.tar.gz" -C darwin/ service-generator)
     (cd $RELEASE_ROOT; tar -czvf "service-generator_linux64-${1}.tar.gz" -C linux_64/ service-generator)
